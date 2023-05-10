bf38897181a7ad788a2d2068cd55c3856dbcf6ee
Steven Bouillon <sbouillon@netfxtech.com>
2023-05-10 15:31:06 -0700

    modifications to pdf flippable

---
+++ flippable_pdf/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "PDF Viewer: Flippable",
+
+    'summary': """
+        Flippable PDF Viewer.""",
+
+    'description': """
+        Flippable PDF Viewer.
+    """,
+
+    'author': "NetFX Tech, LLC",
+    'website': "https://netfxtech.com",
+    'category': 'Tools',
+    'version': '1.0.',
+    'depends': [
+        'base',    ],
+    # 'data': [
+    # ],
+    'license': "LGPL-3",
+    'installable': True,
+}
